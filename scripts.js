e2293baa0c385e4678246f754857bdf71616f1ac
mazenmohamed <mazentohameee@gmail.com>
2025-12-23 01:38:37 +0200

    The final Version Without MarkASReview

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 import questions from './Questions.js';
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm';
 const order = Array.from({ length: questions.length }, (_, i) => i + 1);
 order.sort(() => Math.random() - 0.5);
 let allQuestions = questions;
@@ -13,7 +14,13 @@ let AllQuestionsInHtml = document.getElementsByClassName("question-body");
 let total_index = document.getElementById("total-index");
 let answered_count = document.getElementById("answered-count");
 let timerElement = document.getElementById("timer");
-
+let Results = document.getElementById("quiz-results");
+let ResultsList = document.querySelector(".results-list");
+let FinishExam = document.querySelector("#finish-exam");
+let ScoreResult = document.querySelector("#results-score");
+let sideBar = document.querySelector(".quiz-sidebar");
+let questionmeta = document.querySelector(".question-meta");
+let Answers = [];
 totalcount.innerHTML = allQuestions.length;
 total_index.innerHTML = allQuestions.length;
 window.onload = function(){
@@ -30,6 +37,7 @@ UpdatecurrentQuestionNumber();
 function ShowNextQuestion(questionNumberFromPalette = null) {
     if(currentQuestionIndex >= allQuestions.length - 1){
         Nextbtn.innerHTML = "Submit";
+        Nextbtn.addEventListener("click",sumbittingWithTrue);
         return false;
     }else{
       Nextbtn.innerHTML = "Next";
@@ -61,6 +69,7 @@ allQuestions.forEach(element =>{
 function ShowPrevQuestion(){
   if(currentQuestionIndex > allQuestions.length - 1){
         Nextbtn.innerHTML = "Submit";
+        Nextbtn.removeEventListener("click",sumbittingWithTrue);
     }
     else{
       Nextbtn.innerHTML = "Next";
@@ -74,12 +83,13 @@ function ShowPrevQuestion(){
 }
 Nextbtn.addEventListener("click",ShowNextQuestion);
 prevBtn.addEventListener("click",ShowPrevQuestion);
+FinishExam.addEventListener("click",sumbittingWithTrue);
 function LoadAllQuestions(){
     let i = 0;
     while(i < 10){
         let currentQuestion = allQuestions[order[i] - 1];
         let QuestionBuilder = `
-       <div class="question-body hidden" id="${currentQuestion.id}">
+       <div class="question-body hidden" id="${currentQuestion.id}" data-question-number="${i + 1}">
             <h2 id="q-title-2" class="question-title">${currentQuestion.title}</h2>
             <p class="question-description">Select the correct answer from the choices below.</p>
             <form class="options-form" id="options-form" onsubmit="return false;">
@@ -130,23 +140,98 @@ function LoadAllQuestions(){
       btn.setAttribute("aria-label","Question");
       question_palette.appendChild(btn);
     }
-    
 } 
-function Submitting(){}
+function Submitting(Forced = false) {
+    let AllHtmlQuestions = [...document.getElementsByClassName("question-body")];
+    let theunsolvedQuestions = [];
+  if (Forced == false) {
+  AllHtmlQuestions.forEach(element => {
+      if (element.querySelector("input[type='radio']:checked") == null) {
+        theunsolvedQuestions.push(element.getAttribute("data-question-number"));
+      }
+    });
+    if (theunsolvedQuestions.length > 0) {
+      Swal.fire({
+        title: `You haven't answered questions: ${theunsolvedQuestions.join(", ")}. Do you want to submit?`,
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Submit",
+        denyButtonText: `Don't Submit`
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Submitted", "", "success");
+          IsolatedSubmit();
+        } else if (result.isDenied) {
+          Swal.fire("Go Resolve Them", "", "info");
+        }
+      });
+    }
+  } else {
+    IsolatedSubmit();
+  }
+}
 function Timer(){
   let totalSeconds = 30 * 60;
   const timerInterval = setInterval(() => {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
-
     timerElement.textContent =
         `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-
     if (totalSeconds === 0) {
         clearInterval(timerInterval);
-        Submitting();
+        Submitting(true);
       } else {
         totalSeconds--;
     }
 }, 1000);
+}
+function IsolatedSubmit(){
+  let AllHtmlQuestions = [...document.getElementsByClassName("question-body")];
+AllHtmlQuestions.forEach(element => {
+            allQuestions.forEach(q => {
+              let choosedOnekey =
+                element.querySelector("input[type='radio']:checked")?.value ?? null;
+              let choosedOneText = q.options[choosedOnekey] ?? "";
+              if (q.id == element.id) {
+                Answers.push({
+                  id: q.id,
+                  title: q.title,
+                  correctAnswer: q.correctAnswer,
+                  ChoosedAnswerText: choosedOneText,
+                  ChoosedAnswerKey: choosedOnekey ?? "",
+                  CorrectanswerText: q.options[q.correctAnswer]
+                });
+              }
+
+            });
+          });
+
+          let score = Answers.filter(e =>
+            e.correctAnswer == e.ChoosedAnswerKey
+          ).length;
+          ScoreResult.innerHTML = `Score: ${score} / ${allQuestions.length}`;
+          Answers.forEach(element => {
+            let elementCreator = `
+              <li class="result-item correct">
+                <div class="ri-title">${element.title}</div>
+                <div class="ri-answers">
+                  <div class="ri-chosen">
+                    Your answer: <strong>${element.ChoosedAnswerText}</strong>
+                  </div>
+                  <div class="ri-correct">
+                    Correct: <strong>${element.correctAnswer} ${element.CorrectanswerText}</strong>
+                  </div>
+                </div>
+              </li>`;
+            ResultsList.innerHTML += elementCreator;
+          });
+    QuestionBlock.classList.add("hidden");
+    Results.classList.remove("hidden");
+    Nextbtn.classList.add("hidden");
+    prevBtn.classList.add("hidden");
+    sideBar.classList.add("hidden");
+    questionmeta.style.display = "none";
+  }
+  function sumbittingWithTrue() {
+  Submitting(false);
 }
